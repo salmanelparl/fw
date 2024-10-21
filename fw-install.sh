f3ce653cdccca1c317b81aff0900238484cefc45
salmanelparl <salmannelparl@gmail.com>
2024-10-21 22:27:36 +0700

    Update fw-install.sh

--- fw-install.sh
+++ fw-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Unduh dan salin file fw ke /usr/local/bin
-curl -o ../usr/local/bin/fw -sL https://raw.githubusercontent.com/salmanelparl/fw/main/fw
+curl -o $HOME/.local/bin/fw -sL https://raw.githubusercontent.com/salmanelparl/fw/main/fw
 
 # Berikan izin eksekusi pada file fw
-chmod +x ../usr/local/bin/fw
+chmod +x $HOME/.local/bin/fw
 
 echo "Installation complete. You can run 'fw' now."
